c43c1e5ca45b836ae7421c7faeb53fafa6dfced2
chenyongzhi <chenyongzhijjk@163.com>
2019-12-30 09:16:03 +0800

    Initial commit

---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <template v-slot:center>
+        购物车({{length}})
+      </template>
+    </nav-bar>
+
+    <cart-list/>
+
+    <cart-bottom-bar/>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar"
+  import CartList from "./childComps/CartList"
+  import CartBottomBar from "./childComps/CartBottomBar"
+
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: 'Cart',
+    components: {
+      NavBar,
+      CartList,
+      CartBottomBar
+    },
+    computed: {
+      // ...mapGetters(['cartLength'])
+      ...mapGetters({
+        length: 'cartLength',
+      })
+    },
+  }
+</script>
+
+<style scoped>
+  .cart {
+    height: 100vh;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+    /*font-weight: 700;*/
+  }
+</style>
